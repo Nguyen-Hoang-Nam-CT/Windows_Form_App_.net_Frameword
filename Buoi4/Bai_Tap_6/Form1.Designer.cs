377eebf2e9a7cf2741f17ca467660afe88479a2b
your name <colammoicoan10@gmail.com>
2021-10-22 18:26:21 +0700

    Thêm bài 6 trong buổi 4

---
+++ Buoi4/Bai_Tap_6/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace Bai_Tap_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnMauChu = new System.Windows.Forms.Button();
+            this.btnDong = new System.Windows.Forms.Button();
+            this.btnMauNen = new System.Windows.Forms.Button();
+            this.btnChonFont = new System.Windows.Forms.Button();
+            this.lst1 = new System.Windows.Forms.ListBox();
+            this.txtNhap = new System.Windows.Forms.TextBox();
+            this.txtKQ = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnThem
+            // 
+            this.btnThem.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThem.Location = new System.Drawing.Point(13, 131);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(103, 35);
+            this.btnThem.TabIndex = 1;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXoa.Location = new System.Drawing.Point(197, 131);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(103, 35);
+            this.btnXoa.TabIndex = 2;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            // 
+            // btnMauChu
+            // 
+            this.btnMauChu.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMauChu.Location = new System.Drawing.Point(316, 258);
+            this.btnMauChu.Name = "btnMauChu";
+            this.btnMauChu.Size = new System.Drawing.Size(171, 35);
+            this.btnMauChu.TabIndex = 3;
+            this.btnMauChu.Text = "Chọn màu chữ";
+            this.btnMauChu.UseVisualStyleBackColor = true;
+            this.btnMauChu.Click += new System.EventHandler(this.btnMauChu_Click);
+            // 
+            // btnDong
+            // 
+            this.btnDong.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDong.Location = new System.Drawing.Point(568, 318);
+            this.btnDong.Name = "btnDong";
+            this.btnDong.Size = new System.Drawing.Size(103, 35);
+            this.btnDong.TabIndex = 4;
+            this.btnDong.Text = "Đóng";
+            this.btnDong.UseVisualStyleBackColor = true;
+            this.btnDong.Click += new System.EventHandler(this.btnDong_Click);
+            // 
+            // btnMauNen
+            // 
+            this.btnMauNen.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMauNen.Location = new System.Drawing.Point(534, 258);
+            this.btnMauNen.Name = "btnMauNen";
+            this.btnMauNen.Size = new System.Drawing.Size(171, 35);
+            this.btnMauNen.TabIndex = 5;
+            this.btnMauNen.Text = "Chọn màu nền";
+            this.btnMauNen.UseVisualStyleBackColor = true;
+            this.btnMauNen.Click += new System.EventHandler(this.btnMauNen_Click);
+            // 
+            // btnChonFont
+            // 
+            this.btnChonFont.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChonFont.Location = new System.Drawing.Point(752, 258);
+            this.btnChonFont.Name = "btnChonFont";
+            this.btnChonFont.Size = new System.Drawing.Size(126, 35);
+            this.btnChonFont.TabIndex = 6;
+            this.btnChonFont.Text = "Chọn Font";
+            this.btnChonFont.UseVisualStyleBackColor = true;
+            this.btnChonFont.Click += new System.EventHandler(this.btnChonFont_Click);
+            // 
+            // lst1
+            // 
+            this.lst1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lst1.FormattingEnabled = true;
+            this.lst1.ItemHeight = 25;
+            this.lst1.Location = new System.Drawing.Point(13, 183);
+            this.lst1.Name = "lst1";
+            this.lst1.Size = new System.Drawing.Size(287, 179);
+            this.lst1.TabIndex = 7;
+            // 
+            // txtNhap
+            // 
+            this.txtNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNhap.Location = new System.Drawing.Point(17, 35);
+            this.txtNhap.Name = "txtNhap";
+            this.txtNhap.Size = new System.Drawing.Size(252, 31);
+            this.txtNhap.TabIndex = 8;
+            // 
+            // txtKQ
+            // 
+            this.txtKQ.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKQ.Location = new System.Drawing.Point(316, 12);
+            this.txtKQ.Multiline = true;
+            this.txtKQ.Name = "txtKQ";
+            this.txtKQ.Size = new System.Drawing.Size(564, 240);
+            this.txtKQ.TabIndex = 9;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtNhap);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(13, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(287, 100);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Nhập chuổi";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(912, 380);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtKQ);
+            this.Controls.Add(this.lst1);
+            this.Controls.Add(this.btnChonFont);
+            this.Controls.Add(this.btnMauNen);
+            this.Controls.Add(this.btnDong);
+            this.Controls.Add(this.btnMauChu);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnThem);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnMauChu;
+        private System.Windows.Forms.Button btnDong;
+        private System.Windows.Forms.Button btnMauNen;
+        private System.Windows.Forms.Button btnChonFont;
+        private System.Windows.Forms.ListBox lst1;
+        private System.Windows.Forms.TextBox txtNhap;
+        private System.Windows.Forms.TextBox txtKQ;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Buoi4/Bai_Tap_6/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai_Tap_6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDong_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Do you want to exit?", "Câu hỏi", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                Application.Exit();
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            if (txtNhap.Text == "")
+                MessageBox.Show("Bạn phải nhập vào ô nhập chuổi trước khi bấm thêm", "Thông báo");
+            else
+            {
+                lst1.Items.Add(txtNhap.Text);
+                txtNhap.Clear();
+                txtNhap.Focus();
+            }    
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtNhap.Focus();
+        }
+
+        private void btnMauChu_Click(object sender, EventArgs e)
+        {
+            ColorDialog cld = new ColorDialog();
+            if(cld.ShowDialog() == DialogResult.OK)
+                txtKQ.ForeColor = cld.Color;
+            txtKQ.Text = lst1.SelectedItem.ToString();
+        }
+
+        private void btnMauNen_Click(object sender, EventArgs e)
+        {
+            ColorDialog cld = new ColorDialog();
+            if (cld.ShowDialog() == DialogResult.OK)
+                txtKQ.BackColor = cld.Color;
+        }
+
+        private void btnChonFont_Click(object sender, EventArgs e)
+        {
+            FontDialog fd = new FontDialog();
+            if (fd.ShowDialog() == DialogResult.OK)
+                txtKQ.Font = fd.Font;
+        }
+    }
+}
